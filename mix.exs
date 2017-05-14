04c81a97892f792d9e5931b276fabc3a4d4cf345
Omar Abdelhafith <o.arrabi@me.com>
2017-05-14 22:06:01 +0100

    Rename to ping pong

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ExagentPingPong.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exagent_counter,
+    [app: :exagent_ping_pong,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
